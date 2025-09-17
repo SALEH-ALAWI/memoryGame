14942bef21ffc770817a884b26d638d63d5a3f33
unknown <saleh.alawi.a.a@gmail.com>
2025-09-17 13:24:17 +0300

    add the arrow function for shuffle cards

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ let cardFlipped = false
 let cardOne, cardTwo
 let lockBoard = false
 let score = 0
+
+const shuffleCards = () => {
+  const container = document.querySelector(".cards")
+  const cardArray = Array.from(container.children)
+
+  cardArray.forEach((_, i) => {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[cardArray[i], cardArray[j]] = [cardArray[j], cardArray[i]]
+  })
+
+  cardArray.forEach((card) => container.appendChild(card))
+}
